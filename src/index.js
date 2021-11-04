bebb5d7263f830ba5e773588ad97801df00a2907
mathildetho <mathilde.thoraninth@gmail.com>
2021-11-04 19:18:30 +0100

    crud tasks

--- src/index.js
+++ src/index.js
@@ -48,16 +48,21 @@ const typeDefs = gql`
   }
 
   type Query {
-    myTaskList: [TaskList!]!
+    myTaskLists: [TaskList!]!
+    getTaskList(id: ID!): TaskList!
   }
 
   type Mutation {
-    signUp(input: SignUpInput): AuthUser!
-    signIn(input: SignInInput): AuthUser!
+    signUp(input: SignUpInput!): AuthUser!
+    signIn(input: SignInInput!): AuthUser!
+    createTaskList(title: String!): TaskList!
+    updateTaskList(id: ID!, title: String!): TaskList!
+    deleteTaskList(id: ID!): Boolean!
+    addUserToTaskList(taskListId: ID!, userId: ID!): TaskList!
   }
 `;
 
-const getToken = (user) => jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: '7 days'});
+const getToken = (user) => jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7 days'});
 const getUserFromToken = async (token, db) => {
   if (!token) return null;
   const tokenData = jwt.verify(token, JWT_SECRET);
@@ -67,7 +72,15 @@ const getUserFromToken = async (token, db) => {
 
 const resolvers = {
   Query: {
-    myTaskList: () => []
+    myTaskLists: async (_, __, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+      return await db.collection('Tasks').find({ userIds: user._id }).toArray();
+    },
+    getTaskList: async (_, { id }, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+
+      return await db.collection('Tasks').findOne({ _id: ObjectId(id) });
+    }
   },
   Mutation: {
     signUp: async (_, { input }, { db }) => {
@@ -97,11 +110,54 @@ const resolvers = {
         user,
         token: getToken(user)
       }
-    }
+    },
+    createTaskList: async (_, { title }, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+
+      const newTaskList = {
+        title,
+        createdAt: new Date().toISOString(),
+        userIds: [user._id]
+      }
+
+      const result = await db.collection('Tasks').insertOne(newTaskList);
+      const task = await db.collection('Tasks').findOne({ _id: result.insertedId })
+
+      return task
+    },
+    updateTaskList: async (_, { id, title }, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+      
+      const result = await db.collection('Tasks').findOneAndUpdate({ _id: ObjectId(id) }, { $set: { title } });
+      return result.value;
+    },
+    deleteTaskList: async (_, { id }, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+
+      await db.collection('Tasks').deleteOne({ _id: ObjectId(id) });
+      return true;
+    },
+    addUserToTaskList: async (_, { taskListId, userId }, { db, user }) => {
+      if (!user) throw new Error('Authentication error');
+
+      const task = await db.collection('Tasks').findOne({ _id: ObjectId(taskListId) });
+      if (!task) return null;
+
+      if (task.userIds.find((dbId) => dbId.toString() === userId.toString())) return task;
+
+      await db.collection('Tasks').updateOne({ _id: ObjectId(taskListId) }, { $push: { userIds: ObjectId(userId) } });
+      task.userIds.push(ObjectId(userId));
+      return task;
+    },
   },
 
   User: {
     id: ({ _id, id }) => _id || id
+  },
+  TaskList: {
+    id: ({ _id, id }) => _id || id,
+    progress: () => 0,
+    users: async ({ userIds }, _, { db }) => Promise.all(userIds.map((userId) => db.collection('Users').findOne({ _id: userId })))
   }
 };
 
